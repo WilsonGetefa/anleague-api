b19f0c4a16f1474267e08b7cdfcc928cfcdf15f1
WilsonGetefa <willsonsisimi@gmail.com>
2025-10-23 18:28:33 +0200

    Remove deprecated MongoDB options

--- anleague-nodejs/anleague_app.js
+++ anleague-nodejs/anleague_app.js
@@ -18,7 +18,8 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+//mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.error('MongoDB connection error:', err));
 
