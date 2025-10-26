ae0636392dc47c5bc4933866c707df1ca1709672
WilsonGetefa <willsonsisimi@gmail.com>
2025-10-26 16:47:11 +0200

    updating

--- anleague-nodejs/routes/auth.js
+++ anleague-nodejs/routes/auth.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const User = require('../models/user');
-const jwt = require('jsonwebtoken');
 
 // Signup route
 router.post('/signup', async (req, res) => {
